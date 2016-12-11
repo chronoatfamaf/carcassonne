98ae3cc4c683c561f4fd8a65eda3cf3c2ef150af
IgnaMade <ignamadevollen@gmail.com>
2016-12-11 20:36:18 -0300

    funcionalidad de abandonar partida mientras no ha comenzado, y funcionalidad de redirigir a juego ; implementadas

--- myproyect/partida/views.py
+++ myproyect/partida/views.py
@@ -11,12 +11,15 @@ from .forms import FormularioPartida
 @login_required
 def lista_de_partidas(request):
     usuario = Usuario.objects.get(usuario=request.user.id)
+    # si estaba jugando una partida ya no lo esta asi que lo desunimos de la partida que estaba unido
+    usuario.partida = None
     partidas = Partida.objects.all()
     # asignamos a las partidas las cantidad actual de jugadores que estan esperando por jugarlas
     for partida in partidas:
         partida.jugando = Usuario.objects.filter(partida=partida).count()
         partida.save()
-
+    #Partida.objects.all().delete()
+ 
     return render(request, 'lista_de_partidas.html',
                   {'partidas': partidas})
 
@@ -30,9 +33,34 @@ def unirse_a_partida(request, pk):
     # actualiazmos la cantidad de jugadores que estan jugando la partida
     partida.jugando = Usuario.objects.filter(partida=partida).count()
     partida.save()
+    if partida.jugando != partida.cantidad_jugadores:
+        return render(request,'unirse_a_partida.html', {partida : 'partida'})
+    else:
+        return redirect('jugar_partida', pk=partida.pk) 
 
-    return render(request,'unirse_a_partida.html', {partida : 'partida'})
-    
+
+
+@login_required
+def abandonar_partida(request, pk):
+    partida = Partida.objects.get(pk=pk)
+    usuario = Usuario.objects.get(usuario=request.user.id)
+    # unimos el usuario a la partida
+    usuario.partida = None
+    usuario.save()
+    # actualiazmos la cantidad de jugadores que estan jugando la partida
+    partida.jugando = Usuario.objects.filter(partida=partida).count()
+    partida.save()
+    return redirect('lista_de_partidas') 
+
+
+
+@login_required
+def jugar_partida(request, pk):
+    partida = Partida.objects.get(pk=pk)
+    jugadores = Usuario.objects.filter(partida=partida)
+
+    return render(request,'jugar_partida.html', {jugadores : 'jugadores'}) 
+
 @login_required
 def crear_partida(request):
     if request.method == "POST":
@@ -44,7 +72,7 @@ def crear_partida(request):
             usuario.partida = partida
             usuario.save()
             #pk = partida.pk
-            return render(request, 'unirse_a_partida.html', {partida : 'partida'})
+            return redirect( 'unirse_a_partida',pk=partida.pk)
     else:
         form = FormularioPartida()
         
