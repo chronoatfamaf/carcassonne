baa92f60294922aa70c929a2e5e150afba578fbb
IgnaMade <ignamadevollen@gmail.com>
2016-12-14 02:01:31 -0300

    se elimina codigo muerto

--- myproyect/partida/urls.py
+++ myproyect/partida/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     url(r'^lista_de_partidas/$', views.lista_de_partidas, name='lista_de_partidas'),
     url(r'^unirse_a_partida/(?P<pk>[0-9]+)/$', views.unirse_a_partida, name='unirse_a_partida'),
     url(r'^jugar_partida/$', views.jugar_partida, name='jugar_partida'),
-    url(r'^jugar_partida2/$', views.jugar_partida2, name='jugar_partida2'),
-    url(r'^esperar_turno/(?P<pk>[0-9]+)/$', views.esperar_turno, name='esperar_turno'),
     #url(r'^lista_de_partidas/unirse_a_partida/abandonar_partida/(?P<pk>[0-9]+)/$', views.abandonar_partida, name='abandonar_partida'),
     url(r'^crear_partida/$', views.crear_partida, name='crear_partida'),
 
--- myproyect/partida/views.py
+++ myproyect/partida/views.py
@@ -70,27 +70,6 @@ def abandonar_partida(request, pk):
     return redirect('lista_de_partidas') 
 
 @login_required
-def esperar_turno(request, pk):
-    current_user = Usuario.objects.get(usuario=request.user.id)
-    user = User.objects.get(usuario=current_user)
-    turno = current_user.partida.turnos
-
-    #if turno == current_user.turno:
-     #   return redirect('jugar_partida', pasar=current_user.pk) 
-    context = {
-        'turno' : turno,
-        'current_user' : current_user,
-        'user' : user
-    }
-    print("EN ESPERAR TURNO")
-    print("current_user turno")
-    print(current_user.turno)
-    print("partida turno")
-    print(current_user.partida.turnos)
-    return render(request,'esperar_turno.html', context) 
-    
-
-@login_required
 def jugar_partida(request):
     current_user = Usuario.objects.get(usuario=request.user.id)
     partida = current_user.partida
@@ -124,38 +103,6 @@ def jugar_partida(request):
         return redirect('jugar_partida')
         
     return render(request,'jugar_partida.html', context) 
-
-@login_required
-def jugar_partida2(request):
-    current_user = Usuario.objects.get(usuario=request.user.id)
-    partida = current_user.partida
-    # devuelve los usuarios que estan jugando los partidas
-    usuarios = Usuario.objects.filter(partida=partida)
-    # FLAG PASAR podria ser util sino me sale implementar de esta forma esta funcion
-    pasar = 0
-    print("current_user.turno")
-    print(current_user.turno)
-    print("TURNO ANTEs")
-    print(partida.turnos)
-    #if current_user.turno == partida.turnos + 1 or (current_user.turno == 1 and partida.turnos == 3):
-    #if current_user.turno == partida.turnos:
-    turno = partida.turnos
-    print("TURNO DESPUES")
-    print(turno)
-    # si no se entra al anterior if, entonces se vuelve a renderizar
-    # el template que habia llamado esta funcion
-    # Todo lo anterior es para que un usuario no pueda saltear turnos actualizando la pag desde el browser
-    # Esta consulta devuelve los user de auth asocioados a los usuarios que
-    # estan jugando la partida, es para imprimir los nombres
-    jugadores = User.objects.filter(usuario__partida=partida)
-    context = {
-        'jugadores' : jugadores,
-        'current_user' : current_user,
-        'turno' : turno
-    }
-    if current_user.turno == partida.turnos:
-        return render(request,'jugar_partida.html', context)
-    return render(request,'jugar_partida2.html',context)
 
 @login_required
 def crear_partida(request):
