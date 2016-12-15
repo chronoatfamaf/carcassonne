49e1b665784781ca8b01e7cc9eeb100019c55460
IgnaMade <ignamadevollen@gmail.com>
2016-12-15 20:48:01 -0300

    un commit para ver si funciona algo de git

--- myproyect/partida/models.py
+++ myproyect/partida/models.py
@@ -10,6 +10,22 @@ class Partida(models.Model):
 	# cuando jugando = cantidad_jugadores, la partida i.e juego comienza
 	# default uno porque el usuario que crea la partida la juega tambien
 	jugando = models.IntegerField( default=1)
-	turnos = models.IntegerField(default=1)    
+	turnos = models.IntegerField(default=1)
 
+class Mapa(models.Model):
+	partida = models.ForeignKey(Partida, on_delete=models.CASCADE, blank=True, null=True)
 
+class CeldaMapa(models.Model):
+ 	imagenPieza = models.CharField(default='N', max_length=20)
+ 	esOcupada = models.BooleanField()
+	mapa = models.ForeignKey(Mapa, on_delete=models.CASCADE, blank=True, null=True)
+
+class Pieza(models.Model):
+    numLadoAsociado = models.IntegerField(default=0)
+    esDescartada = models.BooleanField()
+#    ladoA = models.ForeignKey("Pieza", on_delete=models.CASCADE)
+#    ladoB = models.ForeignKey("Pieza", on_delete=models.CASCADE)
+#    ladoC = models.ForeignKey("Pieza", on_delete=models.CASCADE)
+#    ladoD = models.ForeignKey("Pieza", on_delete=models.CASCADE)
+#    Usuario = models.ForeignKey("Usuario", on_delete=models.CASCADE)
+    imagenAsociada =  models.CharField(max_length=9, unique = True)	
