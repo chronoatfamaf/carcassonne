fb1b07c6f24f8177ea4bfc19978f2ecf4cf475ae
martinmonnittola <martinmonnittola@hotmail.com>
2016-12-15 22:02:24 -0300

    Se decidio  creo la vista 'patidacreada' con el fin de  crear la partida, crear los jugadores, cargar la base de datos de Pezas con las 72 piezas correspondientes(y en este proceso se le asigno, a cada una de estas piezas, una unica imagen) de extension PNG guardadas en la carpeta estatic , la cual albergara los achivos estaticos del sistemas que este usarán en el desempeño del mismo), ademas se decidio llevar la clave primaria de la partida al diccionario context que representa el contesto definido para la plantilla que se rellenara, tambien se agregan datos adicionales del objeto partida que representa la partida que sejugara

--- myproyect/JugarPartida/views.py
+++ myproyect/JugarPartida/views.py
@@ -12,4 +12,28 @@ def crearpartida(request):
   context = {}
   return HttpResponse(template.render(context, request))
 
+def partidacreada(request):
+  nombreJugadores = ['Mateo', 'Juan', 'Pedro', 'Marcos', 'Esteban']
+  cantJug = int(request.POST['numeromaximodejugadores'][0])
+  for x in xrange(0, cantJug):
+    J = Jugador(nombre=nombreJugadores[x])
+    J.save()
+  for y in xrange(1, 72):
+    if y < 10:
+      Pieza(esDescartada=False, imagenAsociada ='0' + str(y) + '.png' )
+    else :
+      Pieza(esDescartada=False, imagenAsociada = str(y) + '.png' )
+    
+  P = Partida(nombre = request.POST['nombre'],
+                fechaInicio = str(timezone.now()), esFinalizado = 'N', piezaEnJuego=1)
+  P.save()
+   #Cargamos el template o plantilla
+  template = loader.get_template('JugarPartida/PartidaCreada.html')
 
+  context = {
+  'nombre' : P.nombre,
+  'esFinalizado' : P.esFinalizado,
+  'fechaInicio' : P.fechaInicio,
+  'partidaid' : P.id
+  }
+  return HttpResponse(template.render(context, request))
